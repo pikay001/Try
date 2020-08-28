44c9e82f55c9006b12c20dcc3ec4971fb8698230
PiKay <PiKay@Prakash>
2020-08-28 12:46:11 +0530

    done

---
+++ Test4/src/main/java/com/example/TestAndTryMavenSTS/TestAndTryMavenStsApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestAndTryMavenSTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAndTryMavenStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAndTryMavenStsApplication.class, args);
+	}
+
+}
---
+++ Test4/src/test/java/com/example/TestAndTryMavenSTS/TestAndTryMavenStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestAndTryMavenSTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAndTryMavenStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
